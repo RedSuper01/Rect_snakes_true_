e8112aa0ef00a0765c377284041038e88c0838ee
RedSuper <anastasia.kim.mobile@gmail.com>
2021-12-29 15:39:05 +0600

    Заставка игры(без функционала) Добавлен осмысленный текст заставки

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import os
 FPS = 100
 pygame.init()
-size = width, height = 800, 600
+size = width, height = 900, 700
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
@@ -13,7 +13,7 @@ def terminate():
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = 'data\\' + name
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -32,14 +32,14 @@ def load_image(name, colorkey=None):
 
 def start_screen():
     intro_text = ['Прямоугольники и змейки', '', 'Правила игры',
-                  'Если в правилах несколько строк,'
-                  'приходится выводить их построчно']
-    fon = pygame.transform.scale(load_image('fon.png'), size)
+                  'Начать игру',
+                  'Изменение дизайна']
+    fon = pygame.transform.scale(load_image('fon2.png'), size)
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
+        string_rendered = font.render(line, 1, pygame.Color('blue'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -51,9 +51,8 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                event.type == pygame.MOUSEBUTTONDOWN:
-                return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pass
         pygame.display.flip()
         clock.tick(FPS)
 
